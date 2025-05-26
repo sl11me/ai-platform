c0d0849f7701179f6b3535ace8f5df147a341ba7
sl11me <sl11me@outlook.com>
2025-05-25 23:00:54 -0400

    comment parts to execute workflow tests

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,33 +3,33 @@ resource "azurerm_resource_group" "main" {
   location = var.location
 }
 
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "${var.aks_cluster_name}"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-  dns_prefix          = var.aks_cluster_name
+# resource "azurerm_kubernetes_cluster" "aks" {
+#   name                = "${var.aks_cluster_name}"
+#   location            = azurerm_resource_group.main.location
+#   resource_group_name = azurerm_resource_group.main.name
+#   dns_prefix          = var.aks_cluster_name
 
-  default_node_pool {
-    name       = "default"
-    node_count = 2
-    vm_size    = "Standard_DS2_v2"
-  }
+#   default_node_pool {
+#     name       = "default"
+#     node_count = 2
+#     vm_size    = "Standard_DS2_v2"
+#   }
 
-  identity {
-    type = "SystemAssigned"
-  }
+#   identity {
+#     type = "SystemAssigned"
+#   }
 
-  kubernetes_version = var.kubernetes_version
+#   kubernetes_version = var.kubernetes_version
 
-  tags = {
-    environment = "dev"
-  }
-}
+#   tags = {
+#     environment = "dev"
+#   }
+# }
 
-resource "azurerm_container_registry" "acr" {
-  name                = "${var.azurerm_container_registry_name}"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = var.location
-  sku                 = "Basic"
-  admin_enabled       = true
-}
+# resource "azurerm_container_registry" "acr" {
+#   name                = "${var.azurerm_container_registry_name}"
+#   resource_group_name = azurerm_resource_group.main.name
+#   location            = var.location
+#   sku                 = "Basic"
+#   admin_enabled       = true
+# }
